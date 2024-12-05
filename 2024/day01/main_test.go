1239c1098d0147cd5e03a7cb6d0640a28106e1c3
Su3h7aM <matheusgasource@gmail.com>
2024-12-04 23:06:04 -0300

    test(day01): add tests of part1 and part2

--- 2024/day01/main_test.go
+++ 2024/day01/main_test.go
@@ -1,11 +1,26 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
-func TestHello(t *testing.T) {
-	result := Hello()
+var (
+	left  = []int{3, 4, 2, 1, 3, 3}
+	right = []int{4, 3, 5, 3, 9, 3}
+)
 
-	if result != "hello" {
-		t.Errorf("Hello() should return 'hello', got %s", result)
+func TestPart1(t *testing.T) {
+	total := Part1(left, right)
+
+	if total != 11 {
+		t.Errorf("Part1 must be 31, got %d", total)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	score := Part2(left, right)
+
+	if score != 31 {
+		t.Errorf("Part2 must be 31, got %d", score)
 	}
 }
